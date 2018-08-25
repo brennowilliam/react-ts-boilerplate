653415e89cb91c4aaa38bfc409e444272b87cbdf
brennowilliam <brennowilliam@gmail.com>
2018-08-25 20:59:19 -0300

    Starts to set up prettier configuration

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
+import * as React from 'react'
 
 interface HomeProps {
-  name: string;
+  name: string
 }
 
 export class Home extends React.Component<HomeProps> {
-  render() {
-    return <div>Hello {this.props.name}, How's it going?</div>;
+  public render() {
+    return <div>Hello {this.props.name}, How's it going?</div>
   }
 }
