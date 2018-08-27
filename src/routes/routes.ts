b2dc0e9f6d4389871b8acf141d020feacf692c2c
brennowilliam <brennowilliam@gmail.com>
2018-08-27 15:09:25 -0300

    WIP for the webpack configuration

---
+++ src/routes/routes.ts
@@ -0,0 +1,15 @@
+// Components
+import App from '../components/App'
+import Test from '../components/Test'
+
+// Routes
+export default [
+  {
+    path: '/',
+    component: App,
+  },
+  {
+    path: '/test',
+    component: Test,
+  },
+]
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    filename: './dist/bundle.js',
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-    ],
-  },
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env)
+const webpackMerge = require('webpack-merge')
+
+module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
+  return webpackMerge(
+    {
+      mode,
+      entry: './src/index.tsx',
+      output: {
+        filename: './dist/bundle.js',
+      },
+      devtool: 'source-map',
+      resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+      },
+      module: {
+        rules: [
+          { test: /\.tsx?$/, loader: 'ts-loader' },
+          { test: /\.css$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+        ],
+      },
+      plugins: [new webpack.ProgressPlugin()],
+    },
+    modeConfig(mode)
+  )
 }
